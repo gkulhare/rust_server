224c5149d43bdc3a1cfdf11fedeba78fc81fc7ff
Gaurav <gkulhare@cs.stonybrook.edu>
2024-06-28 18:20:27 -0400

    Add: Post service for keys

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 #![allow(unused)]
 
 use axum::{
-    body::Bytes, extract::{Path, Query, State}, middleware, response::{Html, IntoResponse, Response}, routing::get, Router, http::StatusCode,
+    body::Bytes, 
+    error_handling::HandleErrorLayer, 
+    extract::{DefaultBodyLimit, Path, Query, State}, 
+    handler::Handler, http::StatusCode, middleware, 
+    response::{Html, IntoResponse, Response}, 
+    routing::get, BoxError, Router,
+    debug_handler
+};
+use tower::{
+    ServiceBuilder,
+    make::Shared
 };
 use tower_cookies::CookieManagerLayer;
+use tower_http::{
+    compression::CompressionLayer, limit::RequestBodyLimitLayer, trace::TraceLayer
+};
 use std::{
-    collections::HashMap,
-    sync::{Arc,RwLock},
+    borrow::Cow, collections::HashMap, sync::{Arc,RwLock}, time::Duration
 };
 use serde::{Deserialize, Serialize};
 
@@ -25,6 +37,11 @@ async fn main(){
 
     let state = SharedState::default();
 
+    // Add Faux values to initialize the database
+    state.write().unwrap().db.insert(String::from("share1"), Bytes::from("Today is gonna be the day"));
+    state.write().unwrap().db.insert(String::from("share2"), Bytes::from("That they're gonna throw back to you"));
+    state.write().unwrap().db.insert(String::from("share3"), Bytes::from("By now, you should have somehow realized what you've got to do"));
+
     let router = Router::new()
     .merge(routes_root(Arc::clone(&state)))
     .layer(middleware::map_response(main_response_mapper))
@@ -42,7 +59,7 @@ async fn main_response_mapper(res: Response) -> Response {
     res
 }
 
-fn routes_root(sharedstate: SharedState) -> Router{
+fn routes_root(shared_state: SharedState) -> Router{
     Router::new().route(
         "/hello",
         get(|| async { "Hello World"})
@@ -50,18 +67,34 @@ fn routes_root(sharedstate: SharedState) -> Router{
     // Route to retrieve a value for a key in
     // the key-store db
     .route(
-        "key/:key",
-        get(get_key)
+        "/key/:key",
+        get(get_key.layer(CompressionLayer::new()))
+        .post(
+            set_key
+                // .layer((
+                //     DefaultBodyLimit::disable(),
+                //     RequestBodyLimitLayer::new(1024 * 5_000),
+                // ))
+            )
     )
-    .with_state(Arc::clone(&sharedstate))
+    // .layer(
+    //     ServiceBuilder::new()
+    //         // Handle errors from middleware
+    //         .layer(HandleErrorLayer::new(handle_error))
+    //         .load_shed()
+    //         .concurrency_limit(1024)
+    //         .timeout(Duration::from_secs(10))
+    //         .layer(TraceLayer::new_for_http()),
+    // )
+    .with_state(Arc::clone(&shared_state))
 }
 
 async fn get_key(
     Path(key): Path<String>,
-    State(sharedstate): State<SharedState>,
+    State(shared_state): State<SharedState>,
 ) -> impl IntoResponse {
     
-    let db = &sharedstate.read().unwrap().db;
+    let db = &shared_state.read().unwrap().db;
 
     if let Some(value) = db.get(&key){
         Ok(value.clone())
@@ -70,5 +103,31 @@ async fn get_key(
     }
 }
 
+#[debug_handler]
+async fn set_key(
+    Path(key): Path<String>,
+    State(shared_state): State<SharedState>,
+    bytes: Bytes,
+) -> impl IntoResponse{
+    shared_state.write().unwrap().db.insert(key, bytes);
+}
+
+async fn handle_error(error: BoxError) -> impl IntoResponse {
+    if error.is::<tower::timeout::error::Elapsed>() {
+        return (StatusCode::REQUEST_TIMEOUT, Cow::from("request timed out"));
+    }
+
+    if error.is::<tower::load_shed::error::Overloaded>() {
+        return (
+            StatusCode::SERVICE_UNAVAILABLE,
+            Cow::from("service is overloaded, try again later"),
+        );
+    }
+
+    (
+        StatusCode::INTERNAL_SERVER_ERROR,
+        Cow::from(format!("Unhandled internal error: {error}")),
+    )
+}
 
 
